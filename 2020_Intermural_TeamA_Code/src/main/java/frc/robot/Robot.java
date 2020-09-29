18cd7abc59e88e6101d88be7bc140aa9a92c6f1a
Caleb Wojciechowski <44420919+AKcreepy1@users.noreply.github.com>
2020-09-28 23:31:26 -0700

    Subsystems
    Added Main Subsystems to Robot Code

--- 2020_Intermural_TeamA_Code/src/main/java/frc/robot/Robot.java
+++ 2020_Intermural_TeamA_Code/src/main/java/frc/robot/Robot.java
@@ -8,50 +8,85 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Subsystems.DriveTrain;
+import frc.robot.Subsystems.Shooter;
+import frc.robot.Subsystems.TurnTable;
+
 
 public class Robot extends TimedRobot {
 
-  
+  DriveTrain driveTrain = new DriveTrain();
+  Shooter shooter = new Shooter();
+  TurnTable turnTable = new TurnTable();
+
+
   @Override
   public void robotInit() {
-
+    driveTrain.robotInit();
+    shooter.robotInit();
+    turnTable.robotInit();
   }
 
   @Override
   public void robotPeriodic() {
+    driveTrain.robotPeriodic();
+    shooter.robotPeriodic();
+    turnTable.robotPeriodic();
   }
 
   @Override
   public void autonomousInit() {
+    driveTrain.autonomousInit();
+    shooter.autonomousInit();
+    turnTable.autonomousInit();
   }
 
   @Override
   public void autonomousPeriodic() {
+    driveTrain.autonomousPeriodic();
+    shooter.autonomousPeriodic();
+    turnTable.autonomousPeriodic();
   }
 
   @Override
   public void teleopInit() {
+    driveTrain.teleopInit();
+    shooter.teleopInit();
+    turnTable.teleopInit();
   }
 
   @Override
   public void teleopPeriodic() {
+    driveTrain.teleopPeriodic();
+    shooter.teleopPeriodic();
+    turnTable.teleopPeriodic();
   }
 
   @Override
   public void disabledInit() {
+    driveTrain.disabledInit();
+    shooter.disabledInit();
+    turnTable.disabledInit();
   }
 
   @Override
   public void disabledPeriodic() {
+    driveTrain.disabledPeriodic();
+    shooter.disabledPeriodic();
+    turnTable.disabledPeriodic();
   }
 
   @Override
   public void testInit() {
+    driveTrain.testInit();
+    shooter.testInit();
+    turnTable.testInit();
   }
 
   @Override
   public void testPeriodic() {
+    driveTrain.testPeriodic();
+    shooter.testPeriodic();
+    turnTable.testPeriodic();
   }
 }
---
+++ 2020_Intermural_TeamA_Code/src/main/java/frc/robot/Subsystems/DriveTrain.java
@@ -0,0 +1,54 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.Subsystems;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class DriveTrain extends TimedRobot {
+
+  
+  @Override
+  public void robotInit() {
+  }
+
+  @Override
+  public void robotPeriodic() {
+  }
+
+  @Override
+  public void autonomousInit() {
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+  @Override
+  public void teleopInit() {
+  }
+
+  @Override
+  public void teleopPeriodic() {
+  }
+
+  @Override
+  public void disabledInit() {
+  }
+
+  @Override
+  public void disabledPeriodic() {
+  }
+
+  @Override
+  public void testInit() {
+  }
+
+  @Override
+  public void testPeriodic() {
+  }
+}
