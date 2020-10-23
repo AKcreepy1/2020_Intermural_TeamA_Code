4e5f0b4d30661b13ebe8dfc37d1f58b4a7e13f47
Caleb Wojciechowski <44420919+AKcreepy1@users.noreply.github.com>
2020-10-23 14:49:54 -0700

    Added drive train
    added drive train with a precision  mode

--- 2020_Intermural_TeamA_Code/src/main/java/frc/robot/Subsystems/DriveTrain.java
+++ 2020_Intermural_TeamA_Code/src/main/java/frc/robot/Subsystems/DriveTrain.java
@@ -7,13 +7,53 @@
 
 package frc.robot.Subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+
+import frc.robot.utils.ControllerMap;
+import frc.robot.utils.RobotMap;
 
 public class DriveTrain extends TimedRobot {
 
-  
+  double controllerXAxis;
+  double controllerYAxis;
+
+  double leftSidePower;
+  double rightSidePower;
+
+  double precisionValue;
+
+  boolean precisionEnabled;
+  boolean buttonHasBeenReleased;
+
+  XboxController controller;
+
+  Talon frTalonSR;
+  Talon brTalonSR;
+  WPI_TalonSRX flMasterTalon;
+  WPI_VictorSPX blFollowerTalon;
+
+  public DriveTrain(XboxController omniscientController) {
+    controller = omniscientController;
+  }
+
   @Override
   public void robotInit() {
+
+      frTalonSR = new Talon(RobotMap.DriveTrainMap.frontRightID);
+      brTalonSR = new Talon(RobotMap.DriveTrainMap.backRightID);
+
+      flMasterTalon = new WPI_TalonSRX(RobotMap.DriveTrainMap.frontLeftID);
+      blFollowerTalon = new WPI_VictorSPX(RobotMap.DriveTrainMap.backLeftID);
+      blFollowerTalon.follow(flMasterTalon);
+
+      precisionEnabled = false;
+      buttonHasBeenReleased = true;
   }
 
   @Override
@@ -34,6 +74,27 @@ public class DriveTrain extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
+
+    if (controller.getRawButtonPressed(ControllerMap.rightStickButton) && buttonHasBeenReleased == true) {
+      precisionEnabled = !precisionEnabled;
+      buttonHasBeenReleased = false;
+      precisionValue = 10.0;
+    } else if (controller.getRawButtonReleased(ControllerMap.rightStickButton)) {
+      buttonHasBeenReleased = true;
+      precisionValue = 1.0;
+    }
+
+    controllerXAxis = controller.getRawAxis(ControllerMap.xJoystickAxis);
+    controllerYAxis = controller.getRawAxis(ControllerMap.yJoystickAxis);
+
+    leftSidePower = (controllerYAxis + controllerXAxis) / precisionValue;
+    rightSidePower = (controllerYAxis - controllerXAxis) / precisionValue;
+
+    frTalonSR.set(rightSidePower);
+    brTalonSR.set(leftSidePower);
+
+    flMasterTalon.set(ControlMode.PercentOutput, leftSidePower);
+
   }
 
   @Override
